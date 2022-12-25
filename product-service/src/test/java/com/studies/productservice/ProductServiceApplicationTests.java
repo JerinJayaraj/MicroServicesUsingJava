8261fa3e9950846653ab6ff8a27e4ab49b76caf5
Jerin <jerin@dexlock.com>
2022-12-26 00:17:41 +0530

    test case for createProduct

--- product-service/src/test/java/com/studies/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/studies/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,59 @@
 package com.studies.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.studies.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	@Autowired
+	private MockMvc mockMvc;
+//	@Autowired
+//	private ObjectMapper objectMapper;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 	@Test
-	void contextLoads() {
+	void testMethodForCreateProductEndPoint() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String productRequestJson =  objectMapper.writeValueAsString(productRequest);
+		System.out.println(productRequestJson);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestJson))
+				.andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iPhone 13")
+				.description("iPhone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 
 }
